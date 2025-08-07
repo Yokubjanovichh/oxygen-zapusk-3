713265f9ccec517347ceeb885271edda624412b9
Yokubjanovich <yokubjanovich@gmail.com>
2025-08-08 01:09:19 +0500

    finish

--- src/pages/lastPage.jsx
+++ src/pages/lastPage.jsx
@@ -1,10 +1,60 @@
 import React from "react";
+import camIcon from "../assets/icons/cam-icon.svg";
+import lastPageStepImg from "../assets/icons/lastStepIcon.svg";
+import tgChannel from "../assets/icons/tgChannel.svg";
+import lastPageText from "../assets/img/lastPageText.webp";
+import dateOfConversation from "../assets/img/date.webp";
 import styles from "./last.module.css";
 
 export default function lastPage() {
   return (
     <div className={styles.mainWrapper}>
-      <div className={styles.wrapper}></div>
+      <div className={styles.wrapper}>
+        <div className={styles.navbarHeader}>
+          <img src={camIcon} alt="camIcon" />
+          <p>Online • 20:00 • 22-Avgust</p>
+        </div>
+        <img
+          className={styles.stepImg}
+          src={lastPageStepImg}
+          alt="lastPageStepImg"
+        />
+        <img
+          className={styles.lastPageText}
+          src={lastPageText}
+          alt="lastPageText"
+        />
+
+        <div className={styles.lastButtons}>
+          <button
+            className={styles.tgChannel}
+            onClick={() => (window.location.href = "https://t.me/your_channel")}
+          >
+            <img src={tgChannel} alt="tgChannel" />
+          </button>
+
+          <div className={styles.descMain}>
+            <div className={styles.descChild}>
+              <p>
+                Onlayn taqdimotda qatnashish uchun Telegram kanalimizga obuna
+                bo'ling!
+              </p>
+            </div>
+          </div>
+          <p className={styles.lastPromo}>
+            Chegirma narxlar faqat online taqdimotda qatnashganlarga beriladi!
+          </p>
+          <img
+            className={styles.dateOfConversation}
+            src={dateOfConversation}
+            alt="dateOfConversation"
+          />
+          <p className={styles.footerDesc}>
+            22-Avgust, soat kechki 20:00 da Telegram kanalda jonli efir,
+            qarindoshlaringizam qatnashyapti, qolib ketmang
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
