7c7561af4c419ad37384e4b2783b4897e520c258
Yokubjanovich <yokubjanovich@gmail.com>
2025-08-10 22:06:55 +0500

    adding tg chanell

--- src/pages/lastPage.jsx
+++ src/pages/lastPage.jsx
@@ -49,7 +49,9 @@ export default function LastPage() {
         <div className={styles.lastButtons}>
           <button
             className={styles.tgChannel}
-            onClick={() => (window.location.href = "https://t.me/your_channel")}
+            onClick={() =>
+              (window.location.href = "https://t.me/+mkbOQ1uzS343Yjky")
+            }
           >
             <img src={tgChannel} alt="tgChannel" />
           </button>
