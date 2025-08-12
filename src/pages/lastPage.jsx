90a79199429a3af28678ef9fab1ff64d0384e2c8
Yokubjanovich <yokubjanovich@gmail.com>
2025-08-12 13:40:46 +0500

    fix meta pixel

--- src/pages/lastPage.jsx
+++ src/pages/lastPage.jsx
@@ -49,16 +49,9 @@ export default function LastPage() {
         <div className={styles.lastButtons}>
           <button
             className={styles.tgChannel}
-            onClick={() => {
-              if (typeof fbq !== "undefined") {
-                fbq("trackCustom", "JoinTelegramChannelClicked", {
-                  buttonText: "Telegram kanalga o'tish",
-                  page: window.location.pathname,
-                });
-              }
-
-              window.location.href = "https://t.me/+mkbOQ1uzS343Yjky";
-            }}
+            onClick={() =>
+              (window.location.href = "https://t.me/+mkbOQ1uzS343Yjky")
+            }
           >
             <img src={tgChannel} alt="tgChannel" />
           </button>
